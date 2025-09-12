bf02c729d07dd6ed80883b1491d564bdd3c342da
François Blarel <francois.blarel.partner@decathlon.com>
2025-09-12 17:18:10 +0200

    feat: init boilerplate

---
+++ src/test/java/com/decathlon/techday/dddworkshop/DddworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decathlon.techday.dddworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
