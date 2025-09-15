3069244c6fc312a545d83c00ed29ac87ea1d518b
Bastien TERRIER <bastien.terrier@gmail.com>
2025-09-15 13:47:24 +0200

    feat(ad): set Ad properties

--- src/main/java/com/decathlon/techday/dddworkshop/domain/Ad.java
+++ src/main/java/com/decathlon/techday/dddworkshop/domain/Ad.java
@@ -5,12 +5,42 @@ import java.util.UUID;
 public class Ad {
 
   private final UUID id;
+  private final float price;
+  private final String name;
+  private final String description;
+  private final AdStatus status;
+  private final int quantity;
 
-  public Ad(UUID id) {
+  public Ad(UUID id, float price, String name, String description, AdStatus status, int quantity) {
     this.id = id;
+    this.price = price;
+    this.name = name;
+    this.description = description;
+    this.status = status;
+    this.quantity = quantity;
   }
 
   public UUID id() {
     return id;
+  }
+
+  public float price() {
+    return price;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String description() {
+    return description;
+  }
+
+  public AdStatus status() {
+    return status;
+  }
+
+  public int quantity() {
+    return quantity;
   }
 }
---
+++ src/main/java/com/decathlon/techday/dddworkshop/domain/AdStatus.java
@@ -0,0 +1,5 @@
+package com.decathlon.techday.dddworkshop.domain;
+
+public enum AdStatus {
+  DRAFT, PUBLISHED, SOLD_OUT, PAUSED;
+}
